ff642d519098ee058a5f5563147530d3319d1214
shionsonozakifan1999 <kevmar2928@gmail.com>
2024-11-18 15:59:27 +0000

    added Arena.java

---
+++ src/main/java/Arena.java
@@ -0,0 +1,7 @@
+public class Arena implements Drawable {
+    static int rows = 11;
+    static int columns = 6;
+    private Puyo grid[][] = new Puyo[rows][columns];
+
+    public void draw() { }
+}
